b190cef5c71783252f3d4a1fcf62457019007e05
Le Vivilet <levivlet@pm.me>
2024-10-21 21:20:25 +0200

    test

--- test/FormatDateStrings.test.ts
+++ test/FormatDateStrings.test.ts
@@ -27,3 +27,21 @@ test('formatDate - some hours ago', () => {
   const hours = 2
   expect(FormatDateStrings.someHoursAgo(hours)).toBe('2 hours ago')
 })
+
+test('formatDate - one day ago', () => {
+  expect(FormatDateStrings.oneDayAgo()).toBe('1 day ago')
+})
+
+test('formatDate - some days ago', () => {
+  const days = 2
+  expect(FormatDateStrings.someDaysAgo(days)).toBe('2 days ago')
+})
+
+test('formatDate - one week ago', () => {
+  expect(FormatDateStrings.oneWeekAgo()).toBe('1 week ago')
+})
+
+test('formatDate - some weeks ago', () => {
+  const weeks = 2
+  expect(FormatDateStrings.someWeeksAgo(weeks)).toBe('2 weeks ago')
+})
