1d89abfdbc19436c251a6d5353299f379b9d81b4
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-21 21:47:08 +0200

    feature: add loadContent function (#10)
    * feature: add loadContent function
    
    * import

--- src/parts/CommandMap/CommandMap.ts
+++ src/parts/CommandMap/CommandMap.ts
@@ -3,6 +3,7 @@ import * as FocusPrevious from '../FocusPrevious/FocusPrevious.ts'
 import * as GetAboutDetailString from '../GetAboutDetailString/GetAboutDetailString.ts'
 import * as GetAboutDetailStringWeb from '../GetAboutDetailStringWeb/GetAboutDetailStringWeb.ts'
 import * as GetAboutVirtualDom from '../GetAboutVirtualDom/GetAboutVirtualDom.ts'
+import * as LoadContent from '../LoadContent/LoadContent.ts'
 import * as Render from '../Render/Render.ts'
 
 export const commandMap = {
@@ -11,5 +12,6 @@ export const commandMap = {
   'About.getDetailString': GetAboutDetailString.getDetailString,
   'About.getDetailStringWeb': GetAboutDetailStringWeb.getDetailStringWeb,
   'About.getVirtualDom': GetAboutVirtualDom.getAboutVirtualDom,
+  'About.loadContent': LoadContent.loadContent,
   'About.render': Render.doRender,
 }
