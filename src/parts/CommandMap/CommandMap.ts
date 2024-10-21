bced3ad1751e82f7db91bc96157b1f894e9b6e48
Le Vivilet <levivlet@pm.me>
2024-10-21 18:59:16 +0200

    export

--- src/parts/CommandMap/CommandMap.ts
+++ src/parts/CommandMap/CommandMap.ts
@@ -1,1 +1,9 @@
-export const commandMap = {}
+import * as GetAboutVirtualDom from '../GetAboutVirtualDom/GetAboutVirtualDom.ts'
+import * as GetAboutDetailStringWeb from '../GetAboutDetailStringWeb/GetAboutDetailStringWeb.ts'
+import * as GetAboutDetailString from '../GetAboutDetailString/GetAboutDetailString.ts'
+
+export const commandMap = {
+  'About.getVirtualDom': GetAboutVirtualDom.getAboutVirtualDom,
+  'About.getDetailStringWeb': GetAboutDetailStringWeb.getDetailStringWeb(),
+  'About.getDetailString': GetAboutDetailString.getDetailString(),
+}
