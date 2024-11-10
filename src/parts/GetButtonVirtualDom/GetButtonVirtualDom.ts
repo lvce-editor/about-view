122e4e0c9e20e4a98cd91bd32c24f97c25601641
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-10 14:04:20 +0100

    feature: add mergeclassnames helper function (#25)

--- src/parts/GetButtonVirtualDom/GetButtonVirtualDom.ts
+++ src/parts/GetButtonVirtualDom/GetButtonVirtualDom.ts
@@ -1,12 +1,14 @@
 import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
+import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 
-export const getPrimaryButtonVirtualDom = (message: string, onClick: string) => {
+export const getPrimaryButtonVirtualDom = (message: string, onClick: string): readonly VirtualDomNode[] => {
   return [
     {
       type: VirtualDomElements.Button,
-      className: `${ClassNames.Button} ${ClassNames.ButtonPrimary}`,
+      className: MergeClassNames.mergeClassNames(ClassNames.Button, ClassNames.ButtonPrimary),
       onClick,
       childCount: 1,
     },
@@ -14,11 +16,11 @@ export const getPrimaryButtonVirtualDom = (message: string, onClick: string) => 
   ]
 }
 
-export const getSecondaryButtonVirtualDom = (message: string, onClick: string) => {
+export const getSecondaryButtonVirtualDom = (message: string, onClick: string): readonly VirtualDomNode[] => {
   return [
     {
       type: VirtualDomElements.Button,
-      className: `${ClassNames.Button} ${ClassNames.ButtonSecondary}`,
+      className: MergeClassNames.mergeClassNames(ClassNames.Button, ClassNames.ButtonSecondary),
       onClick,
       childCount: 1,
     },
