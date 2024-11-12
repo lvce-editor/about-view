b530e7dab97fe77bf51cc7f826f7b22c9145cbb6
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-12 21:22:22 +0100

    feature: remove unused await (#36)

--- src/parts/IpcChild/IpcChild.ts
+++ src/parts/IpcChild/IpcChild.ts
@@ -1,7 +1,7 @@
 import * as IpcChildModule from '../IpcChildModule/IpcChildModule.ts'
 
 export const listen = async ({ method }: { method: number }) => {
-  const module = await IpcChildModule.getModule(method)
+  const module = IpcChildModule.getModule(method)
   const rawIpc = await module.listen()
   if (module.signal) {
     module.signal(rawIpc)
