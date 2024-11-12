99d6d45651aea157f6ace502d1c725c76f4165ed
Le Vivilet <levivlet@pm.me>
2024-11-12 21:02:51 +0100

    feature: format code

--- src/parts/GetDialogVirtualDom/GetDialogVirtualDom.ts
+++ src/parts/GetDialogVirtualDom/GetDialogVirtualDom.ts
@@ -12,7 +12,7 @@ export const getDialogVirtualDom = (
   infoMessage: string,
   okMessage: string,
   copyMessage: string,
-  productName: string
+  productName: string,
 ) => {
   const dom = [
     {
