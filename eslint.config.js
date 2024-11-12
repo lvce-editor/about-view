1ce7a245ac4fb657f4684b97ad21eb8aab868421
Le Vivilet <levivlet@pm.me>
2024-11-12 21:25:27 +0100

    add return type rule

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,15 @@
 import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
 
-export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended, {
-  ignores: ['dist', 'coverage', 'scripts'],
-})
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: ['dist', 'coverage', 'scripts'],
+  },
+  {
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': 'error',
+    },
+  },
+)
