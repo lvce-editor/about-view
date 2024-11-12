2823171bad4de8e34b96a54b34558d559362f187
Le Vivilet <levivlet@pm.me>
2024-11-12 21:39:26 +0100

    lint

--- eslint.config.js
+++ eslint.config.js
@@ -22,6 +22,8 @@ export default tseslint.config(
       '@typescript-eslint/ban-ts-comment': 'off',
       '@typescript-eslint/no-unsafe-member-access': 'off',
       '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
     },
   },
 )
