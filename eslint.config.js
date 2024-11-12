b3b2d8e7567fff8b5e5bc8583b270bb71e358f49
Le Vivilet <levivlet@pm.me>
2024-11-12 21:01:43 +0100

    lint

--- eslint.config.js
+++ eslint.config.js
@@ -2,5 +2,5 @@ import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended, {
-  ignores: ['dist', 'coverage'],
+  ignores: ['dist', 'coverage', 'scripts'],
 })
