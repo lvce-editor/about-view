96ab188d000bfc29fef7bd289fabd9e2084db663
Le Vivilet <levivlet@pm.me>
2024-11-16 19:55:48 +0100

    await

--- src/parts/Listen/Listen.ts
+++ src/parts/Listen/Listen.ts
@@ -2,7 +2,7 @@ import { WebWorkerRpcClient } from '@lvce-editor/rpc'
 import * as CommandMap from '../CommandMap/CommandMap.ts'
 
 export const listen = async (): Promise<void> => {
-  WebWorkerRpcClient.create({
+  await WebWorkerRpcClient.create({
     commandMap: CommandMap.commandMap,
   })
 }
