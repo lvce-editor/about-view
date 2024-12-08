d754a4dc4e74e1a9501d87c4f444248237e649bb
Le Vivilet <levivlet@pm.me>
2024-12-08 22:27:35 +0100

    feature: move renderline into separate file

--- src/parts/GetAboutContentVirtualDom/GetAboutContentVirtualDom.ts
+++ src/parts/GetAboutContentVirtualDom/GetAboutContentVirtualDom.ts
@@ -1,19 +1,8 @@
-import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
-import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
-import { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
+import * as RenderLine from '../RenderLine/RenderLine.ts'
+import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
+import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 
-const br = {
-  type: VirtualDomElements.Br,
-  childCount: 0,
-}
-
-const renderLine = (line: string, index: number): readonly VirtualDomNode[] => {
-  if (index === 0) {
-    return [text(line)]
-  }
-  return [br, text(line)]
-}
 
 export const getAboutContentVirtualDom = (lines: readonly string[]): readonly VirtualDomNode[] => {
   const dom = [
@@ -22,7 +11,7 @@ export const getAboutContentVirtualDom = (lines: readonly string[]): readonly Vi
       className: ClassNames.DialogMessage,
       childCount: lines.length * 2 - 1,
     },
-    ...lines.flatMap(renderLine),
+    ...lines.flatMap(RenderLine.renderLine),
   ]
   return dom
 }
