c9dcc0336dda33c2be30ca636ae0eae1e14fbc68
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-08 22:29:13 +0100

    feature: move renderline into separate file (#72)

---
+++ src/parts/RenderLine/RenderLine.ts
@@ -0,0 +1,15 @@
+import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
+import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
+import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
+
+const br:VirtualDomNode = {
+  type: VirtualDomElements.Br,
+  childCount: 0,
+}
+
+export const renderLine = (line: string, index: number): readonly VirtualDomNode[] => {
+  if (index === 0) {
+    return [text(line)]
+  }
+  return [br, text(line)]
+}
