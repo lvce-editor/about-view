b1ab29575611751a050abfd4e43fb9057bee5a59
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-08 23:00:02 +0100

    feature: move buttons into separate file (#75)

---
+++ src/parts/GetSecondaryButtonVirtualDom/GetSecondaryButtonVirtualDom.ts
@@ -0,0 +1,18 @@
+import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
+import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
+import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
+import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
+
+export const getSecondaryButtonVirtualDom = (message: string, onClick: string, name: string): readonly VirtualDomNode[] => {
+  return [
+    {
+      type: VirtualDomElements.Button,
+      className: MergeClassNames.mergeClassNames(ClassNames.Button, ClassNames.ButtonSecondary),
+      onClick,
+      childCount: 1,
+      name,
+    },
+    text(message),
+  ]
+}
