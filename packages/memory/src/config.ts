7303a26d9e17466e6e0d2c6ba7d4483faf3d5971
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-19 21:09:47 +0100

    feature: use npm package to measure worker memory usage (#100)

--- packages/memory/src/config.ts
+++ packages/memory/src/config.ts
@@ -4,3 +4,5 @@ import { root } from './root.ts'
 export const threshold = 390_000
 
 export const workerPath = join(root, '.tmp/dist/dist/aboutWorkerMain.js')
+
+export const playwrightPath = new URL('../../e2e/node_modules/playwright/index.mjs', import.meta.url).toString()
