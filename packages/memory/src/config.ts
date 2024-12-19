70425294bab7a2b8385f5f8e2077d977c0707c81
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-19 21:07:50 +0100

    feature: reduce max memory usage (#101)

--- packages/memory/src/config.ts
+++ packages/memory/src/config.ts
@@ -1,6 +1,6 @@
 import { join } from 'node:path'
 import { root } from './root.ts'
 
-export const threshold = 400_000
+export const threshold = 390_000
 
 export const workerPath = join(root, '.tmp/dist/dist/aboutWorkerMain.js')
