0ffc8679979df4662e7d4438f45640b3d8c46787
Le Vivilet <levivlet@pm.me>
2024-12-20 22:22:48 +0100

    feature: use page object for e2e tests

--- packages/e2e/src/about.focus-next.js
+++ packages/e2e/src/about.focus-next.js
@@ -1,9 +1,8 @@
 export const name = 'about.click-close'
 
-export const test = async ({ Locator, expect, Command }) => {
+export const test = async ({ Locator, expect, Command, About }) => {
   // arrange
-  // TODO use PageObject
-  await Command.execute('About.showAbout')
+  await About.show()
   const dialogContent = Locator('.DialogContent')
   await expect(dialogContent).toBeVisible()
   const infoIcon = dialogContent.locator('.DialogInfoIcon')
