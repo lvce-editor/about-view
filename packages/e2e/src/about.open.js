9bb139d607c041d7be63face81a8b54386f9a175
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-20 22:24:20 +0100

    feature: use page object for e2e tests (#107)

--- packages/e2e/src/about.open.js
+++ packages/e2e/src/about.open.js
@@ -1,7 +1,7 @@
 export const name = 'about.open'
 
-export const test = async ({ Locator, expect, Command }) => {
-  await Command.execute('About.showAbout')
+export const test = async ({ Locator, expect, About }) => {
+  await About.show()
   const dialogContent = Locator('.DialogContent')
   await expect(dialogContent).toBeVisible()
   const infoIcon = dialogContent.locator('.DialogInfoIcon')
