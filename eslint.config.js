23b1f112b8090ca0fc3184b3686d7e4217cf17da
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-15 18:04:37 +0100

    feature: use npm package for eslint config (#113)

--- eslint.config.js
+++ eslint.config.js
@@ -1,60 +1,8 @@
-import eslint from '@eslint/js'
-import tseslint from 'typescript-eslint'
-import perfectionist from 'eslint-plugin-perfectionist'
+import config from '@lvce-editor/eslint-config'
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
+export default [
+  ...config,
   {
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
+    ignores: ['packages/about-view/src/aboutWorkerMain.ts'],
   },
-  {
-    ignores: [
-      'dist',
-      '.tmp',
-      '**/build/**',
-      '**/coverage/**',
-      '**/server/**',
-      '**/e2e/**',
-      '**/memory/**',
-      '**/test-integration/**',
-      '**/test-integration-util/**',
-      'scripts',
-      'rollup.config.js',
-      'eslint.config.js',
-      'packages/about-view/src/aboutWorkerMain.ts',
-    ],
-  },
-  {
-    rules: {
-      '@typescript-eslint/explicit-function-return-type': 'error',
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/ban-ts-comment': 'off',
-      '@typescript-eslint/no-unsafe-member-access': 'off',
-      '@typescript-eslint/no-unsafe-assignment': 'off',
-      '@typescript-eslint/no-unsafe-call': 'off',
-      '@typescript-eslint/no-unsafe-return': 'off',
-      '@typescript-eslint/no-unsafe-argument': 'off',
-    },
-  },
-  {
-    plugins: {
-      perfectionist,
-    },
-    rules: {
-      'perfectionist/sort-imports': [
-        'error',
-        {
-          type: 'natural',
-          order: 'asc',
-          newlinesBetween: 'never',
-        },
-      ],
-    },
-  },
-)
+]
--- packages/about-view/src/parts/VirtualDomHelpers/VirtualDomHelpers.ts
+++ packages/about-view/src/parts/VirtualDomHelpers/VirtualDomHelpers.ts
@@ -1,5 +1,5 @@
+import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
-import { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 
 export const text = (data: string): VirtualDomNode => {
   return {
