eca055bff54cfa0fd824c31461fa53d72e1472a7
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-25 19:11:07 +0100

    feature: add types for event listeners (#124)
    * feature: add types for event listeners
    
    * lint

--- packages/about-view/src/parts/RenderEventListeners/RenderEventListeners.ts
+++ packages/about-view/src/parts/RenderEventListeners/RenderEventListeners.ts
@@ -1,6 +1,7 @@
+import type { DomEventListener } from '../DomEventListener/DomEventListener.ts'
 import * as DomEventListenersFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 
-export const renderEventListeners = (): readonly any[] => {
+export const renderEventListeners = (): readonly DomEventListener[] => {
   return [
     {
       name: DomEventListenersFunctions.HandleClickOk,
