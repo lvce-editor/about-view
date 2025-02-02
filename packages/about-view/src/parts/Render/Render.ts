f6e26518cc06dfe54bc568a9c883d9137c166ece
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-02 14:05:12 +0100

    feature: simplify rendering (#127)

--- packages/about-view/src/parts/Render/Render.ts
+++ packages/about-view/src/parts/Render/Render.ts
@@ -1,13 +1,8 @@
 import type { AboutState } from '../AboutState/AboutState.ts'
+import * as ApplyRender from '../ApplyRender/ApplyRender.ts'
 import * as Diff from '../Diff/Diff.ts'
-import * as GetRenderer from '../GetRenderer/GetRenderer.ts'
 
 export const doRender = (oldState: AboutState, newState: AboutState): any => {
   const diffResult = Diff.diff(oldState, newState)
-  const commands = []
-  for (const item of diffResult) {
-    const fn = GetRenderer.getRenderer(item)
-    commands.push(fn.apply(oldState, newState))
-  }
-  return commands
+  return ApplyRender.applyRender(oldState, newState, diffResult)
 }
