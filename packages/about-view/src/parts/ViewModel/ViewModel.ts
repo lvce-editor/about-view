99f6f9116cc51ea67ccd794169c7e7b9f56116f1
Le Vivilet <levivlet@pm.me>
2025-02-18 19:46:08 +0100

    feature: add function to create viewModel

---
+++ packages/about-view/src/parts/ViewModel/ViewModel.ts
@@ -0,0 +1,8 @@
+export interface ViewModel {
+  readonly productName: string
+  readonly lines: readonly string[]
+  readonly closeMessage: string
+  readonly okMessage: string
+  readonly copyMessage: string
+  readonly infoMessage: string
+}
