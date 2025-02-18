c1436912c99e7a5abf9376f2f5358bd9ba24bbc8
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-18 19:44:42 +0100

    feature: move dialog rendering into separate file (#142)

--- packages/about-view/src/parts/GetRenderer/GetRenderer.ts
+++ packages/about-view/src/parts/GetRenderer/GetRenderer.ts
@@ -1,30 +1,14 @@
-import type { AboutState } from '../AboutState/AboutState.ts'
 import type { Renderer } from '../Renderer/Renderer.ts'
-import * as AboutStrings from '../AboutStrings/AboutStrings.ts'
 import * as DiffType from '../DiffType/DiffType.ts'
-import * as GetAboutVirtualDom from '../GetAboutVirtualDom/GetAboutVirtualDom.ts'
-import * as GetFocusSelector from '../GetFocusSelector/GetFocusSelector.ts'
-
-const renderDialog = (oldState: AboutState, newState: AboutState): any => {
-  const okMessage = AboutStrings.ok()
-  const copyMessage = AboutStrings.copy()
-  const closeMessage = AboutStrings.closeDialog()
-  const infoMessage = AboutStrings.info()
-  const dom = GetAboutVirtualDom.getAboutVirtualDom(newState.productName, newState.lines, closeMessage, okMessage, copyMessage, infoMessage)
-  return ['Viewlet.setDom2', dom]
-}
-
-const renderFocus = (oldState: AboutState, newState: AboutState): any => {
-  const name = GetFocusSelector.getFocusSelector(newState.focusId)
-  return ['Viewlet.focusElementByName', name]
-}
+import * as RenderDialog from '../RenderDialog/RenderDialog.ts'
+import * as RenderFocus from '../RenderFocus/RenderFocus.ts'
 
 export const getRenderer = (diffType: number): Renderer => {
   switch (diffType) {
     case DiffType.RenderAbout:
-      return renderDialog
+      return RenderDialog.renderDialog
     case DiffType.RenderFocus:
-      return renderFocus
+      return RenderFocus.renderFocus
     default:
       throw new Error('unknown renderer')
   }
