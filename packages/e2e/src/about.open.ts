dbe291036cbc971f2bbc8f99de0a4fba615cb05c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-21 21:58:17 +0100

    update depedencies (#148)

--- packages/e2e/src/about.open.ts
+++ packages/e2e/src/about.open.ts
@@ -3,7 +3,10 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 export const name = 'about.open'
 
 export const test: Test = async ({ Locator, expect, About }) => {
+  // act
   await About.show()
+
+  // assert
   const dialogContent = Locator('.DialogContent')
   await expect(dialogContent).toBeVisible()
   const infoIcon = dialogContent.locator('.DialogInfoIcon')
