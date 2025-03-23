06601971ab38253faaeb3cc1a7fababfd50c47cf
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-23 10:19:54 +0100

    feature: add diff numbers (#170)

--- packages/about-view/src/parts/DiffModules/DiffModules.ts
+++ packages/about-view/src/parts/DiffModules/DiffModules.ts
@@ -2,3 +2,5 @@ import * as DiffAbout from '../DiffAbout/DiffAbout.ts'
 import * as DiffFocus from '../DiffFocus/DiffFocus.ts'
 
 export const modules = [DiffAbout, DiffFocus]
+
+export const numbers = [DiffAbout.diffType, DiffFocus.diffType]
