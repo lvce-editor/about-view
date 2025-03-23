abbbac1d0bc439be1b155ecea4038d4ff97a1a19
Le Vivilet <levivlet@pm.me>
2025-03-23 10:20:54 +0100

    feature: improve diffing

--- packages/about-view/src/parts/Diff/Diff.ts
+++ packages/about-view/src/parts/Diff/Diff.ts
@@ -1,11 +1,12 @@
-import type { AboutState } from '../AboutState/AboutState.ts'
+import { AboutState } from '../AboutState/AboutState.ts'
 import * as DiffModules from '../DiffModules/DiffModules.ts'
 
 export const diff = (oldState: AboutState, newState: AboutState): readonly number[] => {
   const diffResult: number[] = []
-  for (const module of DiffModules.modules) {
-    if (!module.isEqual(oldState, newState)) {
-      diffResult.push(module.diffType)
+  for (let i = 0; i < DiffModules.modules.length; i++) {
+    const fn = DiffModules.modules[i]
+    if (!fn(oldState, newState)) {
+      diffResult.push(DiffModules.numbers[i])
     }
   }
   return diffResult
--- packages/about-view/src/parts/DiffModules/DiffModules.ts
+++ packages/about-view/src/parts/DiffModules/DiffModules.ts
@@ -1,6 +1,6 @@
 import * as DiffAbout from '../DiffAbout/DiffAbout.ts'
 import * as DiffFocus from '../DiffFocus/DiffFocus.ts'
 
-export const modules = [DiffAbout, DiffFocus]
+export const modules = [DiffAbout.isEqual, DiffFocus.isEqual]
 
 export const numbers = [DiffAbout.diffType, DiffFocus.diffType]
