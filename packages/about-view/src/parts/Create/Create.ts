63925eb3d1cb4c917a13680c19c0cdf1ec72dd78
Le Vivilet <levivlet@pm.me>
2025-03-30 16:05:46 +0200

    feature: remove unused create parameters

--- packages/about-view/src/parts/Create/Create.ts
+++ packages/about-view/src/parts/Create/Create.ts
@@ -1,7 +1,7 @@
 import type { AboutState } from '../AboutState/AboutState.ts'
 import * as AboutStates from '../AboutStates/AboutStates.ts'
 
-export const create = (uid: number, x: number, y: number, width: number, height: number): void => {
+export const create = (uid: number): void => {
   const state: AboutState = {
     focusId: 0,
     lines: [],
