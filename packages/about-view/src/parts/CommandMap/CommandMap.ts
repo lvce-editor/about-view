dce25771e4bfe99aa7aeb16382f61012508e5073
Le Vivilet <levivlet@pm.me>
2025-03-30 16:04:07 +0200

    feature: register new diff command

--- packages/about-view/src/parts/CommandMap/CommandMap.ts
+++ packages/about-view/src/parts/CommandMap/CommandMap.ts
@@ -1,3 +1,5 @@
+import * as Create from '../Create/Create.ts'
+import * as Diff2 from '../Diff2/Diff2.ts'
 import * as Diff from '../Diff/Diff.ts'
 import * as FocusNext from '../FocusNext/FocusNext.ts'
 import * as FocusPrevious from '../FocusPrevious/FocusPrevious.ts'
@@ -11,27 +13,33 @@ import * as HandleClickCopy from '../HandleClickCopy/HandleClickCopy.ts'
 import * as HandleClickOk from '../HandleClickOk/HandleClickOk.ts'
 import * as HandleFocusIn from '../HandleFocusIn/HandleFocusIn.ts'
 import * as LoadContent from '../LoadContent/LoadContent.ts'
+import * as Render2 from '../Render2/Render2.ts'
 import * as Render from '../Render/Render.ts'
 import * as RenderEventListeners from '../RenderEventListeners/RenderEventListeners.ts'
 import * as ShowAbout from '../ShowAbout/ShowAbout.ts'
 import * as ShowAboutElectron from '../ShowAboutElectron/ShowAboutElectron.ts'
 
 export const commandMap = {
-  'About.diff': Diff.diff,
+  'About.create': Create.create,
+  'About.diff2': Diff2.diff2,
   'About.focusNext': FocusNext.focusNext,
   'About.focusPrevious': FocusPrevious.focusPrevious,
   'About.getCommandIds': GetCommandIds.getCommandIds,
-  'About.getDetailString': GetAboutDetailString.getDetailString,
-  'About.getDetailStringWeb': GetAboutDetailStringWeb.getDetailStringWeb,
   'About.getKeyBindings': GetKeyBindings.getKeyBindings,
-  'About.getVirtualDom': GetAboutVirtualDom.getAboutVirtualDom,
   'About.handleClickClose': HandleClickClose.handleClickClose,
   'About.handleClickCopy': HandleClickCopy.handleClickCopy,
   'About.handleClickOk': HandleClickOk.handleClickOk,
   'About.handleFocusIn': HandleFocusIn.handleFocusIn,
   'About.loadContent': LoadContent.loadContent,
-  'About.render': Render.doRender,
+  'About.render2': Render2.render2,
   'About.renderEventListeners': RenderEventListeners.renderEventListeners,
   'About.showAbout': ShowAbout.showAbout,
   'About.showAboutElectron': ShowAboutElectron.showAboutElectron,
+
+  // deprecated
+  'About.diff': Diff.diff,
+  'About.render': Render.doRender,
+  'About.getVirtualDom': GetAboutVirtualDom.getAboutVirtualDom,
+  'About.getDetailString': GetAboutDetailString.getDetailString,
+  'About.getDetailStringWeb': GetAboutDetailStringWeb.getDetailStringWeb,
 }
