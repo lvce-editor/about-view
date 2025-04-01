fde2b958f80b077cfd8a7999ce21263b51fb8935
Le Vivilet <levivlet@pm.me>
2025-04-02 00:44:58 +0200

    feature: render focus more functional

--- packages/about-view/src/parts/GetRenderer/GetRenderer.ts
+++ packages/about-view/src/parts/GetRenderer/GetRenderer.ts
@@ -2,6 +2,7 @@ import type { Renderer } from '../Renderer/Renderer.ts'
 import * as DiffType from '../DiffType/DiffType.ts'
 import * as RenderDialog from '../RenderDialog/RenderDialog.ts'
 import * as RenderFocus from '../RenderFocus/RenderFocus.ts'
+import * as RenderFocusContext from '../RenderFocusContext/RenderFocusContext.ts'
 
 export const getRenderer = (diffType: number): Renderer => {
   switch (diffType) {
@@ -9,6 +10,8 @@ export const getRenderer = (diffType: number): Renderer => {
       return RenderDialog.renderDialog
     case DiffType.RenderFocus:
       return RenderFocus.renderFocus
+    case DiffType.RenderFocusContext:
+      return RenderFocusContext.renderFocusContext
     default:
       throw new Error('unknown renderer')
   }
