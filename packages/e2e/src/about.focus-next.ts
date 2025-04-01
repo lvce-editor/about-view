e4fe9dc6f7e3babc015f89fe0054c71e2bbdbca0
Le Vivilet <levivlet@pm.me>
2025-04-01 19:17:32 +0200

    skip

--- packages/e2e/src/about.focus-next.ts
+++ packages/e2e/src/about.focus-next.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'about.click-close'
 
+export const skip = 1
+
 export const test: Test = async ({ Locator, expect, Command, About }) => {
   // arrange
   await About.show()
