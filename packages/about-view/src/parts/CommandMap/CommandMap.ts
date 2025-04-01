ca79fb1a8524642133608386e19269f97a84e0dc
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-01 19:37:49 +0200

    feature: remove deprecated commands (#187)

--- packages/about-view/src/parts/CommandMap/CommandMap.ts
+++ packages/about-view/src/parts/CommandMap/CommandMap.ts
@@ -1,12 +1,8 @@
 import * as Create from '../Create/Create.ts'
 import * as Diff2 from '../Diff2/Diff2.ts'
-import * as Diff from '../Diff/Diff.ts'
 import * as Dispose from '../Dispose/Dispose.ts'
 import * as FocusNext from '../FocusNext/FocusNext.ts'
 import * as FocusPrevious from '../FocusPrevious/FocusPrevious.ts'
-import * as GetAboutDetailString from '../GetAboutDetailString/GetAboutDetailString.ts'
-import * as GetAboutDetailStringWeb from '../GetAboutDetailStringWeb/GetAboutDetailStringWeb.ts'
-import * as GetAboutVirtualDom from '../GetAboutVirtualDom/GetAboutVirtualDom.ts'
 import * as GetCommandIds from '../GetCommandIds/GetCommandIds.ts'
 import * as GetKeyBindings from '../GetKeyBindings/GetKeyBindings.ts'
 import * as HandleClickClose from '../HandleClickClose/HandleClickClose.ts'
@@ -14,9 +10,7 @@ import * as HandleClickCopy from '../HandleClickCopy/HandleClickCopy.ts'
 import * as HandleClickOk from '../HandleClickOk/HandleClickOk.ts'
 import * as HandleFocusIn from '../HandleFocusIn/HandleFocusIn.ts'
 import * as LoadContent2 from '../LoadContent2/LoadContent2.ts'
-import * as LoadContent from '../LoadContent/LoadContent.ts'
 import * as Render2 from '../Render2/Render2.ts'
-import * as Render from '../Render/Render.ts'
 import * as RenderEventListeners from '../RenderEventListeners/RenderEventListeners.ts'
 import * as ShowAbout from '../ShowAbout/ShowAbout.ts'
 import * as ShowAboutElectron from '../ShowAboutElectron/ShowAboutElectron.ts'
@@ -38,12 +32,4 @@ export const commandMap = {
   'About.renderEventListeners': RenderEventListeners.renderEventListeners,
   'About.showAbout': ShowAbout.showAbout,
   'About.showAboutElectron': ShowAboutElectron.showAboutElectron,
-
-  // deprecated
-  'About.loadContent': LoadContent.loadContent,
-  'About.diff': Diff.diff,
-  'About.render': Render.doRender,
-  'About.getVirtualDom': GetAboutVirtualDom.getAboutVirtualDom,
-  'About.getDetailString': GetAboutDetailString.getDetailString,
-  'About.getDetailStringWeb': GetAboutDetailStringWeb.getDetailStringWeb,
 }
