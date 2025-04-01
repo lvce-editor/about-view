9e336b06a1d10d298bf358aa72f68dfb1b5509fc
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-02 00:43:06 +0200

    feature: change order (#189)

--- packages/about-view/src/parts/Render2/Render2.ts
+++ packages/about-view/src/parts/Render2/Render2.ts
@@ -3,7 +3,7 @@ import * as ApplyRender from '../ApplyRender/ApplyRender.ts'
 
 export const doRender = (uid: number, diffResult: readonly number[]): readonly any[] => {
   const { oldState, newState } = AboutStates.get(uid)
-  const commands = ApplyRender.applyRender(oldState, newState, diffResult)
   AboutStates.set(uid, newState, newState)
+  const commands = ApplyRender.applyRender(oldState, newState, diffResult)
   return commands
 }
