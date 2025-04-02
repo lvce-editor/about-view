70537db88d0dc540f3d617d7b1dd1f244208dcdc
Le Vivilet <levivlet@pm.me>
2025-04-02 19:02:33 +0200

    lint

--- packages/about-view/src/parts/AboutStates/AboutStates.ts
+++ packages/about-view/src/parts/AboutStates/AboutStates.ts
@@ -1,33 +1,4 @@
 import type { AboutState } from '../AboutState/AboutState.ts'
+import * as ViewletRegistry from '../ViewletRegistry/ViewletRegistry.ts'
 
-const states = Object.create(null)
-
-export const get = (
-  uid: string | number,
-): {
-  oldState: AboutState
-  newState: AboutState
-} => {
-  return states[uid]
-}
-
-export const remove = (uid: string | number): void => {
-  delete states[uid]
-}
-
-export const clear = (): void => {
-  for (const key of Object.keys(states)) {
-    remove(key)
-  }
-}
-
-export const getKeys = (): readonly string[] => {
-  return Object.keys(states)
-}
-
-export const set = (uid: string | number, oldState: AboutState, newState: AboutState): void => {
-  states[uid] = {
-    oldState,
-    newState,
-  }
-}
+export const { get, set, dispose, getKeys } = ViewletRegistry.create<AboutState>()
