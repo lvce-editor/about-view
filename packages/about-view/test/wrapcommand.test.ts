4d5abb06f7cbd55446edf373c599f7c4327fd410
Le Vivilet <levivlet@pm.me>
2025-04-02 19:12:06 +0200

    test

---
+++ packages/about-view/test/wrapcommand.test.ts
@@ -0,0 +1,40 @@
+import { jest, test, expect } from '@jest/globals'
+import { wrapCommand } from '../src/parts/WrapCommand/WrapCommand.js'
+import * as AboutStates from '../src/parts/AboutStates/AboutStates.js'
+import type { AboutState } from '../src/parts/AboutState/AboutState.js'
+
+const createState = (uid: number): AboutState => ({
+  productName: 'test',
+  lines: [],
+  focusId: 0,
+  uid,
+})
+
+test('wrapCommand should update state when function returns new state', async () => {
+  const uid = 123
+  const oldState = createState(uid)
+  const newState = { ...oldState, productName: 'new' }
+
+  AboutStates.set(uid, oldState, oldState)
+
+  const fn = async (state: AboutState): Promise<AboutState> => newState
+  const wrapped = wrapCommand(fn)
+  await wrapped(uid)
+
+  const { newState: currentState } = AboutStates.get(uid)
+  expect(currentState).toEqual(newState)
+})
+
+test('wrapCommand should not update state when function returns same state', async () => {
+  const uid = 123
+  const state = createState(uid)
+
+  AboutStates.set(uid, state, state)
+
+  const fn = async (state: AboutState): Promise<AboutState> => state
+  const wrapped = wrapCommand(fn)
+  await wrapped(uid)
+
+  const { newState: currentState } = AboutStates.get(uid)
+  expect(currentState).toEqual(state)
+})
