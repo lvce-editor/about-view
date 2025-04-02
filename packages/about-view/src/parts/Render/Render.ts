c4069d74e8e491d1fa71dded884aed91b1f28a90
Le Vivilet <levivlet@pm.me>
2025-04-02 19:17:35 +0200

    feature: remove unused code

--- packages/about-view/src/parts/Render/Render.ts
+++
@@ -1,8 +0,0 @@
-import type { AboutState } from '../AboutState/AboutState.ts'
-import * as ApplyRender from '../ApplyRender/ApplyRender.ts'
-import * as Diff from '../Diff/Diff.ts'
-
-export const doRender = (oldState: AboutState, newState: AboutState): any => {
-  const diffResult = Diff.diff(oldState, newState)
-  return ApplyRender.applyRender(oldState, newState, diffResult)
-}
