507e0576adaa24dda9f823bb6345d01f0ef8e913
Le Vivilet <levivlet@pm.me>
2025-05-24 09:55:51 +0200

    lint

--- packages/about-view/src/parts/HandleClickButton/HandleClickButton.ts
+++ packages/about-view/src/parts/HandleClickButton/HandleClickButton.ts
@@ -1,8 +1,8 @@
 import type { AboutState } from '../AboutState/AboutState.ts'
-import * as InputName from '../InputName/InputName.ts'
+import * as HandleClickClose from '../HandleClickClose/HandleClickClose.ts'
 import * as HandleClickCopy from '../HandleClickCopy/HandleClickCopy.ts'
 import * as HandleClickOk from '../HandleClickOk/HandleClickOk.ts'
-import * as HandleClickClose from '../HandleClickClose/HandleClickClose.ts'
+import * as InputName from '../InputName/InputName.ts'
 
 export const handleClickButton = async (state: AboutState, name: string): Promise<AboutState> => {
   switch (name) {
