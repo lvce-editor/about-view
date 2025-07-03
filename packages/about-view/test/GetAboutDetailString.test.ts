75cfd4067aaa104842efd407cc6816025c807d5f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-03 20:37:05 +0200

    feature: simplify string test (#258)

--- packages/about-view/test/GetAboutDetailString.test.ts
+++ packages/about-view/test/GetAboutDetailString.test.ts
@@ -1,45 +1,29 @@
-import { beforeEach, expect, jest, test } from '@jest/globals'
-
-beforeEach(() => {
-  jest.resetAllMocks()
-})
-
-jest.unstable_mockModule('../src/parts/Process/Process.ts', () => {
-  return {
-    getElectronVersion(): string {
-      return '0.0.0-dev'
-    },
-    getNodeVersion(): string {
-      return '0.0.0-dev'
-    },
-    getChromeVersion(): string {
-      return '0.0.0-dev'
-    },
-    getVersion(): string {
-      return '0.0.0-dev'
-    },
-    getCommit(): string {
-      return 'abc'
-    },
-    getV8Version(): string {
-      return '0.0.0-dev'
-    },
-    getDate(): string {
-      return 'n/a'
-    },
-  }
-})
-
-const GetAboutDetailString = await import('../src/parts/GetAboutDetailString/GetAboutDetailString.ts')
+import { expect, test } from '@jest/globals'
+import { MockRpc } from '@lvce-editor/rpc'
+import { RendererWorker } from '@lvce-editor/rpc-registry'
+import * as GetAboutDetailString from '../src/parts/GetAboutDetailString/GetAboutDetailString.ts'
 
 test('getDetailStringWeb', async () => {
+  const mockRpc = MockRpc.create({
+    commandMap: {},
+    invoke: (method: string) => {
+      if (method === 'Process.getElectronVersion') {
+        return '0.0.0-dev'
+      }
+      if (method === 'Process.getNodeVersion') {
+        return '0.0.0-dev'
+      }
+      if (method === 'Process.getChromeVersion') {
+        return '0.0.0-dev'
+      }
+      if (method === 'Process.getV8Version') {
+        return '0.0.0-dev'
+      }
+      throw new Error('unexpected method ' + method)
+    },
+  })
+  RendererWorker.set(mockRpc)
   expect(await GetAboutDetailString.getDetailString()).toBe(
-    `Version: 0.0.0-dev
-Commit: abc
-Date: Invalid Date: n/a
-Electron: 0.0.0-dev
-Chromium: 0.0.0-dev
-Node: 0.0.0-dev
-V8: 0.0.0-dev`,
+    `Version: 0.0.0-dev\nCommit: unknown commit\nDate: unknown\nElectron: 0.0.0-dev\nChromium: 0.0.0-dev\nNode: 0.0.0-dev\nV8: 0.0.0-dev`,
   )
 })
