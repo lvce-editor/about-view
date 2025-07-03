1c0fc28e41ca9d5c1bf610df5579065ed02bd0ca
Le Vivilet <levivlet@pm.me>
2025-07-03 21:53:50 +0200

    lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,3 @@
 import * as config from '@lvce-editor/eslint-config'
 
-export default [
-  ...config.default,
-  {
-    rules: {
-      'jest/no-restricted-jest-methods': 'off',
-      '@typescript-eslint/prefer-readonly-parameter-types': 'off',
-    },
-  },
-]
+export default [...config.default]
