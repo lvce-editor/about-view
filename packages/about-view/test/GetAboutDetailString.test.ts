c816f312ab92207112844120aac56950a8a7c71a
Le Vivilet <levivlet@pm.me>
2025-07-03 20:35:11 +0200

    feature: simplify imports

--- packages/about-view/test/GetAboutDetailString.test.ts
+++ packages/about-view/test/GetAboutDetailString.test.ts
@@ -4,6 +4,34 @@ import { RendererWorker } from '@lvce-editor/rpc-registry'
 import * as GetAboutDetailString from '../src/parts/GetAboutDetailString/GetAboutDetailString.ts'
 
 test('getDetailStringWeb', async () => {
+  const mockRpc = MockRpc.create({
+    commandMap: {},
+    invoke: (method: string) => {
+      if (method === 'Process.getElectronVersion') {
+        return '0.0.0-dev'
+      }
+      if (method === 'Process.getNodeVersion') {
+        return '0.0.0-dev'
+      }
+      if (method === 'Process.getChromeVersion') {
+        return '0.0.0-dev'
+      }
+      if (method === 'Process.getVersion') {
+        return '0.0.0-dev'
+      }
+      if (method === 'Process.getCommit') {
+        return 'abc'
+      }
+      if (method === 'Process.getV8Version') {
+        return '0.0.0-dev'
+      }
+      if (method === 'Process.getDate') {
+        return 'n/a'
+      }
+      throw new Error(`unexpected method ${method}`)
+    },
+  })
+  RendererWorker.set(mockRpc)
   expect(await GetAboutDetailString.getDetailString()).toBe(
     `Version: 0.0.0-dev
 Commit: abc
