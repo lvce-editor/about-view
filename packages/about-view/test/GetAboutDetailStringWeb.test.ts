925a8e3c5440f9a474bf3bc8fa57a8ef938e431a
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-03 21:34:58 +0200

    feature: simplify detail string test (#264)

--- packages/about-view/test/GetAboutDetailStringWeb.test.ts
+++ packages/about-view/test/GetAboutDetailStringWeb.test.ts
@@ -1,32 +1,11 @@
-import { beforeEach, expect, jest, test } from '@jest/globals'
-
-beforeEach(() => {
-  jest.resetAllMocks()
-})
-
-jest.unstable_mockModule('../src/parts/Process/Process.ts', () => {
-  return {
-    version: '0.0.0-dev',
-    commit: 'abc',
-    date: 'n/a',
-  }
-})
-
-jest.unstable_mockModule('../src/parts/GetBrowser/GetBrowser.ts', () => {
-  return {
-    getBrowser(): string {
-      return 'Browser: Node.js/22'
-    },
-  }
-})
-
-const GetAboutDetailStringWeb = await import('../src/parts/GetAboutDetailStringWeb/GetAboutDetailStringWeb.ts')
+import { expect, test } from '@jest/globals'
+import * as GetAboutDetailStringWeb from '../src/parts/GetAboutDetailStringWeb/GetAboutDetailStringWeb.ts'
 
 test('getDetailStringWeb', () => {
   expect(GetAboutDetailStringWeb.getDetailStringWeb()).toEqual([
     'Version: 0.0.0-dev',
-    'Commit: abc',
-    'Date: Invalid Date: n/a',
-    'Browser: Browser: Node.js/22',
+    'Commit: unknown commit',
+    'Date: unknown',
+    'Browser: Node.js/22',
   ])
 })
