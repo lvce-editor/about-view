b63e7c83d1e99f1cda79e7be950d5fd1f3799992
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-03 21:40:14 +0200

    feature: mock navigator in tests (#266)

--- packages/about-view/test/GetAboutDetailStringWeb.test.ts
+++ packages/about-view/test/GetAboutDetailStringWeb.test.ts
@@ -1,11 +1,13 @@
-import { expect, test } from '@jest/globals'
+import { expect, test, beforeAll } from '@jest/globals'
 import * as GetAboutDetailStringWeb from '../src/parts/GetAboutDetailStringWeb/GetAboutDetailStringWeb.ts'
 
+beforeAll(() => {
+  // @ts-expect-error
+  globalThis.navigator = {
+    userAgent: 'Test',
+  }
+})
+
 test('getDetailStringWeb', () => {
-  expect(GetAboutDetailStringWeb.getDetailStringWeb()).toEqual([
-    'Version: 0.0.0-dev',
-    'Commit: unknown commit',
-    'Date: unknown',
-    'Browser: Node.js/22',
-  ])
+  expect(GetAboutDetailStringWeb.getDetailStringWeb()).toEqual(['Version: 0.0.0-dev', 'Commit: unknown commit', 'Date: unknown', 'Browser: Test'])
 })
