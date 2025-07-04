02fa7d41ff9b1708acf2d250010a8b7183c4545f
Le Vivilet <levivlet@pm.me>
2025-07-04 09:54:50 +0200

    feature: remove unused code

--- packages/about-view/src/parts/DomEventListenerFunctions/DomEventListenerFunctions.ts
+++ packages/about-view/src/parts/DomEventListenerFunctions/DomEventListenerFunctions.ts
@@ -1,6 +1,4 @@
 export const HandleClickClose = 1
-export const HandleClickCopy = 2
-export const HandleClickOk = 3
 export const HandleClickButton = 4
 export const HandleContextMenu = 5
 export const HandleFocusIn = 6
