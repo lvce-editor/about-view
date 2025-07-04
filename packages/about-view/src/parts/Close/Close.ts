e950794b3bf8431456f50824b7e51016fef9f802
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-04 10:01:00 +0200

    feature: use existing string (#272)

--- packages/about-view/src/parts/Close/Close.ts
+++ packages/about-view/src/parts/Close/Close.ts
@@ -1,5 +1,6 @@
 import * as CloseWidget from '../CloseWidget/CloseWidget.ts'
+import * as ViewletModuleId from '../ViewletModuleId/ViewletModuleId.ts'
 
 export const close = async (): Promise<void> => {
-  await CloseWidget.closeWidget('About')
+  await CloseWidget.closeWidget(ViewletModuleId.About)
 }
