873abfb134de2a24605e90342b1a1426eebebb93
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-04 09:58:52 +0200

    feature: use virtual dom helper functions (#271)

--- packages/about-view/src/parts/GetAboutVirtualDom/GetAboutVirtualDom.ts
+++ packages/about-view/src/parts/GetAboutVirtualDom/GetAboutVirtualDom.ts
@@ -1,10 +1,10 @@
-import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
+import type { VirtualDomNode} from '@lvce-editor/virtual-dom-worker';
+import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 import * as GetAboutContentVirtualDom from '../GetAboutContentVirtualDom/GetAboutContentVirtualDom.ts'
 import * as GetDialogVirtualDom from '../GetDialogVirtualDom/GetDialogVirtualDom.ts'
 import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
-import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 
 export const getAboutVirtualDom = (
   productName: string,
