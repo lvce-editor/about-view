b3cb798d2ec03b9d61f7743f72f52c99a841dc51
Le Vivilet <levivlet@pm.me>
2025-09-01 20:06:52 +0200

    format

--- packages/about-view/src/parts/GetDialogVirtualDom/GetDialogVirtualDom.ts
+++ packages/about-view/src/parts/GetDialogVirtualDom/GetDialogVirtualDom.ts
@@ -1,4 +1,4 @@
-import type { VirtualDomNode} from '@lvce-editor/virtual-dom-worker';
+import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import * as AriaBoolean from '../AriaBoolean/AriaBoolean.ts'
 import * as AriaRoles from '../AriaRoles/AriaRoles.ts'
