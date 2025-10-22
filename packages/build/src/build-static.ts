7791d915078fc01b3b414b5503f84647b0308fd9
Le Vivilet <levivilet@pm.me>
2025-10-22 17:31:00 +0200

    fix

--- packages/build/src/build-static.ts
+++ packages/build/src/build-static.ts
@@ -27,9 +27,9 @@ const content = await readFile(rendererWorkerPath, 'utf8')
 const workerPath = join(root, '.tmp/dist/dist/aboutViewWorkerMain.js')
 const remoteUrl = getRemoteUrl(workerPath)
 
-const occurrence = `// const aboutWorkerUrl = \`\${assetDir}/packages/about-view/dist/aboutWorkerMain.js\`
+const occurrence = `// const aboutWorkerUrl = \`\${assetDir}/packages/about-view-worker/dist/aboutWorkerMain.js\`
 const aboutWorkerUrl = \`${remoteUrl}\``
-const replacement = `const aboutWorkerUrl = \`\${assetDir}/packages/about-view/dist/aboutWorkerMain.js\``
+const replacement = `const aboutWorkerUrl = \`\${assetDir}/packages/about-view-worker/dist/aboutWorkerMain.js\``
 if (!content.includes(occurrence)) {
   throw new Error('occurrence not found')
 }
