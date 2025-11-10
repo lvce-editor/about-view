c6e71369c1bc229a52e83b453af2acdff3ecdbb8
Le Vivilet <levivilet@pm.me>
2025-11-10 09:46:40 +0100

    feature: update variables for better treeshaking (#334)
    * feature: update variables for better treeshaking
    
    * lint

--- packages/about-view/src/parts/GetAboutDetailStringWeb/GetAboutDetailStringWeb.ts
+++ packages/about-view/src/parts/GetAboutDetailStringWeb/GetAboutDetailStringWeb.ts
@@ -1,11 +1,12 @@
+import { commit } from '../Commit/Commit.ts'
+import { commitDate } from '../CommitDate/CommitDate.ts'
 import * as FormatAboutDate from '../FormatAboutDate/FormatAboutDate.ts'
 import * as GetBrowser from '../GetBrowser/GetBrowser.ts'
-import * as Process from '../Process/Process.ts'
+import { version } from '../Version/Version.ts'
 
 export const getDetailStringWeb = (): readonly string[] => {
-  const { date, commit, version } = Process
   const now = Date.now()
-  const formattedDate = FormatAboutDate.formatAboutDate(date, now)
+  const formattedDate = FormatAboutDate.formatAboutDate(commitDate, now)
   const browser = GetBrowser.getBrowser()
   const lines = [`Version: ${version}`, `Commit: ${commit}`, `Date: ${formattedDate}`, `Browser: ${browser}`]
   return lines
