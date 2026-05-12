7ef9e09670212da2eaafab35baa1f83c58279158
Le Vivilet <levivilet@pm.me>
2026-05-13 01:16:46 +0200

    feature: refactor date formatting functions and add tests

--- packages/about-view/src/parts/FormatDateFuture/FormatDateFuture.ts
+++ packages/about-view/src/parts/FormatDateFuture/FormatDateFuture.ts
@@ -1,52 +1,53 @@
 // based on https://github.com/microsoft/vscode/blob/bd782eb059e133d3a20fdb446b8feb0010a278ad/src/vs/base/common/date.ts (License MIT)
 import * as FormatDateStrings from '../FormatDateStrings/FormatDateStrings.ts'
+import * as FormatRelativeDate from '../FormatRelativeDate/FormatRelativeDate.ts'
 import { day, hour, minute, month, week, year } from '../TimeUnit/TimeUnit.ts'
 
+const ranges = [
+  {
+    divisor: 1,
+    limit: minute,
+    one: FormatDateStrings.inOneSecond,
+    some: FormatDateStrings.inSomeSeconds,
+  },
+  {
+    divisor: minute,
+    limit: hour,
+    one: FormatDateStrings.inOneMinute,
+    some: FormatDateStrings.inSomeMinutes,
+  },
+  {
+    divisor: hour,
+    limit: day,
+    one: FormatDateStrings.inOneHour,
+    some: FormatDateStrings.inSomeHours,
+  },
+  {
+    divisor: day,
+    limit: week,
+    one: FormatDateStrings.inOneDay,
+    some: FormatDateStrings.inSomeDays,
+  },
+  {
+    divisor: week,
+    limit: month,
+    one: FormatDateStrings.inOneWeek,
+    some: FormatDateStrings.inSomeWeeks,
+  },
+  {
+    divisor: month,
+    limit: year,
+    one: FormatDateStrings.inOneMonth,
+    some: FormatDateStrings.inSomeMonths,
+  },
+  {
+    divisor: year,
+    limit: Number.POSITIVE_INFINITY,
+    one: FormatDateStrings.inOneYear,
+    some: FormatDateStrings.inSomeYears,
+  },
+] as const
+
 export const formatDateFuture = (seconds: number): string => {
-  if (seconds < minute) {
-    if (seconds === 1) {
-      return FormatDateStrings.inOneSecond()
-    }
-    return FormatDateStrings.inSomeSeconds(seconds)
-  }
-  if (seconds < hour) {
-    const minutes = Math.floor(seconds / minute)
-    if (minutes === 1) {
-      return FormatDateStrings.inOneMinute()
-    }
-    return FormatDateStrings.inSomeMinutes(minutes)
-  }
-  if (seconds < day) {
-    const days = Math.floor(seconds / hour)
-    if (days === 1) {
-      return FormatDateStrings.inOneHour()
-    }
-    return FormatDateStrings.inSomeHours(days)
-  }
-  if (seconds < week) {
-    const days = Math.floor(seconds / day)
-    if (days === 1) {
-      return FormatDateStrings.inOneDay()
-    }
-    return FormatDateStrings.inSomeDays(days)
-  }
-  if (seconds < month) {
-    const weeks = Math.floor(seconds / week)
-    if (weeks === 1) {
-      return FormatDateStrings.inOneWeek()
-    }
-    return FormatDateStrings.inSomeWeeks(weeks)
-  }
-  if (seconds < year) {
-    const months = Math.floor(seconds / month)
-    if (months === 1) {
-      return FormatDateStrings.inOneMonth()
-    }
-    return FormatDateStrings.inSomeMonths(months)
-  }
-  const years = Math.floor(seconds / year)
-  if (years === 1) {
-    return FormatDateStrings.inOneYear()
-  }
-  return FormatDateStrings.inSomeYears(years)
+  return FormatRelativeDate.formatRelativeDate(seconds, ranges)
 }
--- packages/about-view/src/parts/FormatDatePast/FormatDatePast.ts
+++ packages/about-view/src/parts/FormatDatePast/FormatDatePast.ts
@@ -1,52 +1,53 @@
 // based on https://github.com/microsoft/vscode/blob/bd782eb059e133d3a20fdb446b8feb0010a278ad/src/vs/base/common/date.ts (License MIT)
 import * as FormatDateStrings from '../FormatDateStrings/FormatDateStrings.ts'
+import * as FormatRelativeDate from '../FormatRelativeDate/FormatRelativeDate.ts'
 import { minute, hour, day, week, month, year } from '../TimeUnit/TimeUnit.ts'
 
+const ranges = [
+  {
+    limit: minute,
+    divisor: 1,
+    one: FormatDateStrings.oneSecondAgo,
+    some: FormatDateStrings.someSecondsAgo,
+  },
+  {
+    limit: hour,
+    divisor: minute,
+    one: FormatDateStrings.oneMinuteAgo,
+    some: FormatDateStrings.someMinutesAgo,
+  },
+  {
+    limit: day,
+    divisor: hour,
+    one: FormatDateStrings.oneHourAgo,
+    some: FormatDateStrings.someHoursAgo,
+  },
+  {
+    limit: week,
+    divisor: day,
+    one: FormatDateStrings.oneDayAgo,
+    some: FormatDateStrings.someDaysAgo,
+  },
+  {
+    limit: month,
+    divisor: week,
+    one: FormatDateStrings.oneWeekAgo,
+    some: FormatDateStrings.someWeeksAgo,
+  },
+  {
+    limit: year,
+    divisor: month,
+    one: FormatDateStrings.oneMonthAgo,
+    some: FormatDateStrings.someMonthsAgo,
+  },
+  {
+    limit: Number.POSITIVE_INFINITY,
+    divisor: year,
+    one: FormatDateStrings.oneYearAgo,
+    some: FormatDateStrings.someYearsAgo,
+  },
+] as const
+
 export const formatDatePast = (seconds: number): string => {
-  if (seconds < minute) {
-    if (seconds === 1) {
-      return FormatDateStrings.oneSecondAgo()
-    }
-    return FormatDateStrings.someSecondsAgo(seconds)
-  }
-  if (seconds < hour) {
-    const minutes = Math.floor(seconds / minute)
-    if (minutes === 1) {
-      return FormatDateStrings.oneMinuteAgo()
-    }
-    return FormatDateStrings.someMinutesAgo(minutes)
-  }
-  if (seconds < day) {
-    const days = Math.floor(seconds / hour)
-    if (days === 1) {
-      return FormatDateStrings.oneHourAgo()
-    }
-    return FormatDateStrings.someHoursAgo(days)
-  }
-  if (seconds < week) {
-    const days = Math.floor(seconds / day)
-    if (days === 1) {
-      return FormatDateStrings.oneDayAgo()
-    }
-    return FormatDateStrings.someDaysAgo(days)
-  }
-  if (seconds < month) {
-    const weeks = Math.floor(seconds / week)
-    if (weeks === 1) {
-      return FormatDateStrings.oneWeekAgo()
-    }
-    return FormatDateStrings.someWeeksAgo(weeks)
-  }
-  if (seconds < year) {
-    const months = Math.floor(seconds / month)
-    if (months === 1) {
-      return FormatDateStrings.oneMonthAgo()
-    }
-    return FormatDateStrings.someMonthsAgo(months)
-  }
-  const years = Math.floor(seconds / year)
-  if (years === 1) {
-    return FormatDateStrings.oneYearAgo()
-  }
-  return FormatDateStrings.someYearsAgo(years)
+  return FormatRelativeDate.formatRelativeDate(seconds, ranges)
 }
