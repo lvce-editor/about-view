723aaff25aaf8e22de4d8694753edd512df6bff7
Le Vivilet <levivilet@pm.me>
2026-06-25 12:41:55 +0200

    run e2e tests in firefox and webkit (#377)
    * run e2e tests in firefox and webkit
    
    * tsc

--- packages/about-view/src/parts/Diff2/Diff2.ts
+++ packages/about-view/src/parts/Diff2/Diff2.ts
@@ -2,7 +2,7 @@ import * as AboutStates from '../AboutStates/AboutStates.ts'
 import * as Diff from '../Diff/Diff.ts'
 
 export const diff2 = (uid: number): readonly number[] => {
-  const { newState, oldState } = AboutStates.get(uid)
-  const diffResult = Diff.diff(oldState, newState)
+  const { oldState, scheduledState } = AboutStates.get(uid)
+  const diffResult = Diff.diff(oldState, scheduledState)
   return diffResult
 }
