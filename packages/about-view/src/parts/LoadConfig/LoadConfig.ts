eba6e2741a2c1d3433e7feffe74ce89bce77bfbd
Le Vivilet <levivilet@pm.me>
2026-06-29 21:44:58 +0200

    feature: improve loadConfig

--- packages/about-view/src/parts/LoadConfig/LoadConfig.ts
+++ packages/about-view/src/parts/LoadConfig/LoadConfig.ts
@@ -1,4 +1,6 @@
+import { FileSystemWorker } from '@lvce-editor/rpc-registry'
 import type { AboutState } from '../AboutState/AboutState.ts'
+import { getConfigJsonPath } from '../GetConfigJsonPath/GetConfigJsonPath.ts'
 
 interface Config {
   readonly commit: string
@@ -7,10 +9,14 @@ interface Config {
 }
 
 export const loadConfig = async (state: AboutState): Promise<Config> => {
+  const configJsonPath = await getConfigJsonPath()
+  const text = await FileSystemWorker.readFile(configJsonPath)
+  // TODO handle error
+  const data = JSON.parse(text)
   // TODO
   return {
-    commit: '',
-    date: '',
-    version: '',
+    commit: data.commit,
+    date: data.date,
+    version: data.version,
   }
 }
