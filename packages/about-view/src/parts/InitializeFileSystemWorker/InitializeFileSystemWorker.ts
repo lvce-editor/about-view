d69c8df90eab3ebd7ba4595d0841485dc3328a58
Le Vivilet <levivilet@pm.me>
2026-06-29 21:06:32 +0200

    test

--- packages/about-view/src/parts/InitializeFileSystemWorker/InitializeFileSystemWorker.ts
+++ packages/about-view/src/parts/InitializeFileSystemWorker/InitializeFileSystemWorker.ts
@@ -1,7 +1,7 @@
 import { LazyTransferMessagePortRpcParent } from '@lvce-editor/rpc'
 import { FileSystemWorker, RendererWorker } from '@lvce-editor/rpc-registry'
 
-const send = async (port: MessagePort): Promise<void> => {
+export const send = async (port: MessagePort): Promise<void> => {
   await RendererWorker.sendMessagePortToFileSystemWorker(port, 0)
 }
 
