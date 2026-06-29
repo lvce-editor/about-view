9cbecf80cc99304760c00899d7cab6e17c507ccc
Le Vivilet <levivilet@pm.me>
2026-06-29 17:20:33 +0200

    init

--- packages/about-view/src/parts/InitializeFileSystemWorker/InitializeFileSystemWorker.ts
+++ packages/about-view/src/parts/InitializeFileSystemWorker/InitializeFileSystemWorker.ts
@@ -5,7 +5,7 @@ const send = async (port: MessagePort): Promise<void> => {
   await RendererWorker.sendMessagePortToFileSystemWorker(port, 0)
 }
 
-export const initializeRendererWorker = async (): Promise<void> => {
+export const initializeFileSystemWorker = async (): Promise<void> => {
   const rpc = await LazyTransferMessagePortRpcParent.create({
     commandMap: {},
     send,
