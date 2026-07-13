f0ae36a854fa65cf651cc4eda86e2e39ee1cc44d
Le Vivilet <levivilet@pm.me>
2026-07-13 08:03:39 +0000

    fix: update type memory budget

--- packages/memory/src/config.ts
+++ packages/memory/src/config.ts
@@ -3,7 +3,7 @@ import { root } from './root.ts'
 
 export const threshold = 440_000
 
-export const instantiations = 1101
+export const instantiations = 1297
 
 export const instantiationsPath = join(root, 'packages', 'about-view')
 
