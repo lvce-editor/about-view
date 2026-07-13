fbe874168aa5dd9fc14f54dfb34aaab2798c523b
Le Vivilet <levivilet@pm.me>
2026-07-13 09:48:26 +0200

    feature: destructure view model (#405)

--- packages/about-view/src/parts/RenderDialog/RenderDialog.ts
+++ packages/about-view/src/parts/RenderDialog/RenderDialog.ts
@@ -3,14 +3,7 @@ import * as CreateViewModel from '../CreateViewModel/CreateViewModel.ts'
 import * as GetAboutVirtualDom from '../GetAboutVirtualDom/GetAboutVirtualDom.ts'
 
 export const renderDialog = (oldState: AboutState, newState: AboutState): readonly any[] => {
-  const viewModel = CreateViewModel.createViewModel(newState)
-  const dom = GetAboutVirtualDom.getAboutVirtualDom(
-    viewModel.productName,
-    viewModel.lines,
-    viewModel.closeMessage,
-    viewModel.okMessage,
-    viewModel.copyMessage,
-    viewModel.infoMessage,
-  )
+  const { closeMessage, copyMessage, infoMessage, lines, okMessage, productName } = CreateViewModel.createViewModel(newState)
+  const dom = GetAboutVirtualDom.getAboutVirtualDom(productName, lines, closeMessage, okMessage, copyMessage, infoMessage)
   return ['Viewlet.setDom2', dom]
 }
