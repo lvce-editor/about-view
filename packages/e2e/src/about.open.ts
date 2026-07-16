27a5de0d721840b71c834e785777031ed017cb0c
Le Vivilet <levivilet@pm.me>
2026-07-16 10:46:34 +0200

    feature: expand about interaction e2e coverage (#411)

--- packages/e2e/src/about.open.ts
+++ packages/e2e/src/about.open.ts
@@ -3,8 +3,6 @@ import { assertAboutContent, closeAbout, openAbout } from './_about.js'
 
 export const name = 'about.open'
 
-export const skip = 1
-
 export const test: Test = async (api) => {
   const dialogContent = await openAbout(api)
   try {
