8729cadd41a47abefcb651c2c5acdf216380b8bc
Le Vivilet <levivilet@pm.me>
2026-07-28 08:27:58 +0000

    feature: move memory measurement to build

---
+++ packages/build/src/measureMemory.ts
@@ -0,0 +1,21 @@
+import { measureMemory } from '@lvce-editor/measure-memory'
+import { join } from 'node:path'
+import { root } from './root.ts'
+
+const threshold = 460_000
+
+const instantiations = 1317
+
+const instantiationsPath = join(root, 'packages', 'about-view')
+
+const workerPath = join(root, '.tmp/dist/dist/aboutWorkerMain.js')
+
+const playwrightPath = import.meta.resolve('../../../node_modules/playwright/index.mjs')
+
+await measureMemory({
+  playwrightPath,
+  workerPath,
+  threshold,
+  instantiations,
+  instantiationsPath,
+})
