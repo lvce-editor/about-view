db85949a036d12e68bff2e2ad61200e2f2df8bca
levivilet <72156503+levivilet@users.noreply.github.com>
2026-09-12 10:46:22 +0000

    fix: remove unused notification worker dependency

--- packages/build/src/build.ts
+++ packages/build/src/build.ts
@@ -1,7 +1,6 @@
 import { execa } from 'execa'
 import { cp, mkdir, readFile, rm, writeFile } from 'node:fs/promises'
 import { join } from 'node:path'
-import { fileURLToPath } from 'node:url'
 import { bundleJs } from './bundleJs.ts'
 import { root } from './root.ts'
 
@@ -54,8 +53,6 @@ await rm(dist, { recursive: true, force: true })
 await mkdir(dist, { recursive: true })
 
 await bundleJs()
-
-await cp(fileURLToPath(import.meta.resolve('@lvce-editor/notification-center-view')), join(dist, 'dist', 'notificationCenterWorkerMain.js'))
 
 const version = await getVersion()
 
