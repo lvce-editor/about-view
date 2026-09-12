0847c205ae3978538a97d1ef87cfd14bc63027da
levivilet <72156503+levivilet@users.noreply.github.com>
2026-09-12 11:13:17 +0000

    fix: share the linked server entrypoint with development

--- packages/build/src/dev.ts
+++ packages/build/src/dev.ts
@@ -7,7 +7,7 @@ const main = async (): Promise<void> => {
     cwd: root,
     stdio: 'inherit',
   })
-  execa('node', ['node_modules/@lvce-editor/server/bin/server.js', '--test-path=packages/e2e', '--link=.tmp/dist'], {
+  execa('node', ['packages/server/src/server.js', '--test-path=packages/e2e'], {
     cwd: root,
     stdio: 'inherit',
   })
