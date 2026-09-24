b45a6831b3a832ef943a6a47179d32d32fde11df
Le Vivilet <levivilet@pm.me>
2026-09-24 00:46:16 -0700

    fix: pass file uris to linked server (#483)

--- packages/server/src/server.js
+++ packages/server/src/server.js
@@ -1,6 +1,3 @@
-import { fileURLToPath } from 'node:url'
-
-const workerPath = fileURLToPath(new URL('../../../.tmp/dist', import.meta.url))
-process.argv.push(`--link=${workerPath}`)
+process.argv.push(`--link=${new URL('../../../.tmp/dist', import.meta.url).href}`)
 
 await import('@lvce-editor/server/bin/server.js')
